8828ccd7dc625cd05ace75837a0d0f4a38522d84
henro47 <53427025+henro47@users.noreply.github.com>
2021-09-27 12:08:10 +0200

    Started with prereq page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss',]
 })
 export class AppComponent {
   title = 'ITRI625-Metasploit-Blog';
+  navTitle = 'Metasploit' ;
   navHome = 'Home' ;
   navPreReq = '1. Prerequisites' ;
   navScenOne = '2. Scenario 1' ;
--- src/app/component/prereq/prereq.component.ts
+++ src/app/component/prereq/prereq.component.ts
@@ -9,6 +9,18 @@ export class PrereqComponent implements OnInit {
 
   constructor() { }
 
+  cardTitle = "Metasploit Prerequisites" ;
+  cardSubtitle = "The following is required for this tutorial" ;
+
+  vmWare = "VMware Workstation Player" ;
+  vmWareLink = "sdasd";
+
+  metasploit = "metasploit" ;
+  metasploitLink = "" ;
+
+  metasploitTable = "Metasploit Table" ;
+  metasploitTableLink = "" ;
+
   ngOnInit(): void {
   }
 
